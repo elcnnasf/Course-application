62dace3039e2a3cf04ac344e3ef1e58696336640
elcan <elcan.atashoff@gmail.com>
2025-11-10 18:05:16 +0400

    Adding Controller

---
+++ CourseApplication/Controllers/GroupController.cs
@@ -0,0 +1,235 @@
+﻿using Domain.Models;
+using Service.Helpers;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseApplication.Controllers
+{
+    public class GroupController
+    {
+        private void CreateGroup(GroupService group)
+        {
+            Console.WriteLine("_______Create Group_______");
+
+            Console.Write("Enter group name: ");
+            string name = Console.ReadLine();
+            string teacher;
+            while (true)
+            {
+                Console.Write("Enter teacher name: ");
+                teacher = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(teacher) && teacher.All(char.IsLetter))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Name must contain only letters!");
+                Console.Beep();
+            }
+
+            int roomNumber;
+            while (true)
+            {
+                Console.Write("Enter room number: ");
+                string roomInput = Console.ReadLine();
+                if (int.TryParse(roomInput, out roomNumber))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Input must be a number!");
+                Console.Beep();
+            }
+
+            Groups groups = new Groups
+            {
+                name = name,
+                teacher = teacher,
+                room = roomNumber
+            };
+
+            group.Create(groups);
+            Helpers.ConsoleColor(ConsoleColor.Green, "Group created successfully!");
+            Console.WriteLine($"Name: {name} | Teacher: {teacher} | Room: {roomNumber}");
+            Console.ReadKey();
+        }
+
+
+
+        private void UpdateGroup(GroupService groupService)
+        {
+            Console.WriteLine("_______Update Group_______");
+            int id;
+            while (true)
+            {
+                Console.Write("Enter group ID to update: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+                Console.Beep();
+            }
+            Console.Write("Enter new group name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter new teacher name: ");
+            string teacher = Console.ReadLine();
+
+            int room;
+            while (true)
+            {
+                Console.Write("Enter new room number: ");
+                if (int.TryParse(Console.ReadLine(), out room))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+                Console.Beep();
+            }
+            Groups updatedGroup = new Groups
+            {
+                name = name,
+                teacher = teacher,
+                room = room
+            };
+            try
+            {
+                groupService.Update(id, updatedGroup);
+            }
+            catch (Exception ex)
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{ex.Message}");
+                Console.ResetColor();
+            }
+            Console.ReadKey();
+        }
+
+
+
+        private void GetGroupById(GroupService groupService)
+        {
+            Console.WriteLine("_______Get Group by ID_______");
+
+            int id;
+            while (true)
+            {
+                Console.Write("Enter group ID: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+                Console.Beep();
+            }
+
+            var group = groupService.Get(id);
+            if (group != null)
+            {
+                Console.WriteLine($"ID: {group.Id} | Name: {group.name} | Teacher: {group.teacher} | Room: {group.room} ");
+            }
+            Console.ReadKey();
+        }
+
+
+        private void DeleteGroup(GroupService group)
+        {
+            Console.WriteLine("_______Delete Group_______");
+
+            int id;
+            while (true)
+            {
+                Console.Write("Enter group ID to delete: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+                Console.Beep();
+            }
+
+            group.Delete(id);
+
+            Console.ReadKey();
+        }
+
+        private void GetGroupsByTeacher(GroupService group)
+        {
+            Console.WriteLine("_______Get Groups by Teacher_______");
+
+            Console.Write("Enter teacher name: ");
+            string teacher = Console.ReadLine();
+
+            var groups = group.GetAll().FindAll(g => g.teacher.Equals(teacher, StringComparison.OrdinalIgnoreCase));
+
+            if (groups.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No groups found for this teacher.");
+            else
+            {
+                foreach (var g in groups)
+                    Console.WriteLine($"ID: {g.Id} | Name: {g.name} | Room: {g.room}");
+            }
+            Console.ReadKey();
+        }
+
+        private void GetGroupsByRoom(GroupService group)
+        {
+            Console.WriteLine("_______Get Groups by Room_______");
+
+            int room;
+            while (true)
+            {
+                Console.Write("Enter room number: ");
+                if (int.TryParse(Console.ReadLine(), out room))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+                Console.Beep();
+            }
+
+            var groups = group.GetAll().FindAll(g => g.room == room);
+
+            if (groups.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No groups found in this room.");
+            else
+            {
+                foreach (var g in groups)
+                    Console.WriteLine($"ID: {g.Id} | Name: {g.name} | Teacher: {g.teacher}");
+            }
+            Console.ReadKey();
+        }
+
+        private void GetAllGroups(GroupService group)
+        {
+            Console.WriteLine("_______All Groups_______");
+
+            var allGroups = group.GetAll();
+
+            if (allGroups.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No groups found.");
+            else
+            {
+                foreach (var g in allGroups)
+                    Console.WriteLine($"ID: {g.Id} | Name: {g.name} | Teacher: {g.teacher} | Room: {g.room}");
+            }
+            Console.ReadKey();
+        }
+
+        private void SearchGroupsByName(GroupService service)
+        {
+            Console.WriteLine("_______Search Groups by Name_______");
+
+            Console.Write("Enter a name: ");
+            string keyword = Console.ReadLine();
+            var groups = service.GetAll().Where(g => g.name.ToLower().Contains(keyword.ToLower())).ToList();
+
+            if (groups.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No groups found.");
+            else
+            {
+                foreach (var g in groups)
+                    Console.WriteLine($"ID: {g.Id} | Name: {g.name} | Teacher: {g.teacher} | Room: {g.room}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ CourseApplication/Controllers/StudentController.cs
@@ -0,0 +1,285 @@
+﻿using Domain.Models;
+using Service.Helpers;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseApplication.Controllers
+{
+    public class StudentController
+    {
+        private void CreateStudent(StudentService studentService, GroupService groupService)
+        {
+            Console.WriteLine("_______Create Student_______");
+
+            string name;
+            while (true)
+            {
+                Console.Write("Enter student name: ");
+                name = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(name) && name.All(char.IsLetter))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Name must contain only letters!");
+                Console.Beep();
+            }
+
+            string surname;
+            while (true)
+            {
+                Console.Write("Enter student surname: ");
+                surname = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(surname) && surname.All(char.IsLetter))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Surname must contain only letters!");
+                Console.Beep();
+            }
+
+            int age;
+            while (true)
+            {
+                Console.Write("Enter student age: ");
+                if (int.TryParse(Console.ReadLine(), out age))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Input must be a number!");
+                Console.Beep();
+            }
+
+            Console.Write("Enter group name: ");
+            string groupName = Console.ReadLine();
+
+            var exGroup = groupService.GetAll().FirstOrDefault(g => g.name.ToLower() == groupName.ToLower());
+
+            if (exGroup is null)
+            {
+                Console.Beep();
+                Helpers.ConsoleColor(ConsoleColor.Red, $"Group with name {groupName} does not exist!");
+                Console.ReadKey();
+                return;
+            }
+            Student student = new Student
+            {
+                name = name,
+                surname = surname,
+                age = age,
+                group = exGroup
+            };
+            studentService.Create(student);
+            Helpers.ConsoleColor(ConsoleColor.Green, "Student created successfully!");
+            Console.WriteLine($"Name: {name} | Surname: {surname} | Age: {age} | Group: {exGroup.name}");
+            Console.ReadKey();
+        }
+
+
+
+        private void UpdateStudent(StudentService studentService, GroupService groupService)
+        {
+            Console.WriteLine("_______Update Student_______");
+
+            int id;
+            while (true)
+            {
+                Console.Write("Enter student ID: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Input must be a number!");
+            }
+            var existing = studentService.Get(id);
+            if (existing == null)
+            {
+                Console.ReadKey();
+                return;
+            }
+            string name;
+            while (true)
+            {
+                Console.Write("Enter new student name: ");
+                name = Console.ReadLine()?.Trim();
+                if (!string.IsNullOrWhiteSpace(name) && name.All(char.IsLetter))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Name must contain only letters!");
+                Console.Beep();
+            }
+            string surname;
+            while (true)
+            {
+                Console.Write("Enter new student surname: ");
+                surname = Console.ReadLine()?.Trim();
+                if (!string.IsNullOrWhiteSpace(surname) && surname.All(char.IsLetter))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Surname must contain only letters!");
+            }
+            int age;
+            while (true)
+            {
+                Console.Write("Enter new student age: ");
+                if (int.TryParse(Console.ReadLine(), out age) && age > 0)
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Age must be a positive number!");
+            }
+            Groups exGroup;
+            while (true)
+            {
+                Console.Write("Enter new group name: ");
+                string groupName = Console.ReadLine().Trim();
+                exGroup = groupService.GetAll().FirstOrDefault(g => g.name.Equals(groupName, StringComparison.OrdinalIgnoreCase));
+
+                if (exGroup != null)
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, $"Group with name '{groupName}' does not exist!");
+            }
+
+            Student student = new Student
+            {
+                name = name,
+                surname = surname,
+                age = age,
+                group = exGroup
+            };
+
+            try
+            {
+                studentService.Update(id, student);
+                Helpers.ConsoleColor(ConsoleColor.Green, "Student updated successfully!");
+            }
+            catch (Exception ex)
+            {
+                Helpers.ConsoleColor(ConsoleColor.Red, $"{ex.Message}");
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+        private void GetStudentById(StudentService studentService, GroupService groupService)
+        {
+            Console.WriteLine("_______Get Student by ID_______");
+
+            int id;
+            while (true)
+            {
+                Console.Write("Enter student ID: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+            }
+
+            var student = studentService.Get(id);
+            if (student != null)
+            {
+
+                Console.WriteLine($"Name :{student.name} | Surname: {student.surname} | Age: {student.age} | Group: {student.group.name} ");
+            }
+            Console.ReadKey();
+        }
+
+
+        private void DeleteStudent(StudentService studentService)
+        {
+            Console.WriteLine("_______Delete Student_______");
+
+            int id;
+            while (true)
+            {
+                Console.Write("Enter student ID to delete: ");
+                if (int.TryParse(Console.ReadLine(), out id))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+            }
+
+            studentService.Delete(id);
+
+            Console.ReadKey();
+        }
+
+
+        private void GetStudentByAge(StudentService studentService)
+        {
+            Console.WriteLine("_______Get student by age_______");
+
+            int age;
+            while (true)
+            {
+                Console.Write("Enter new student age: ");
+                if (int.TryParse(Console.ReadLine(), out age))
+                    break;
+                else
+                    Helpers.ConsoleColor(ConsoleColor.Red, "Input must be a number!");
+            }
+            var students = studentService.GetAll().FindAll(s => s.age == age);
+            if (students.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No Student found for this age.");
+            else
+            {
+                foreach (var s in students)
+                    Console.WriteLine($"Name :{s.name} | Surname: {s.surname} | Age: {s.age} | Group: {s.group.name} ");
+            }
+            Console.ReadKey();
+        }
+        private void GetAllStudentByGroupId(StudentService studentService)
+        {
+            Console.WriteLine("_______Get all students by group id_______");
+
+            int groupId;
+            while (true)
+            {
+                Console.Write("Enter group id: ");
+                if (int.TryParse(Console.ReadLine(), out groupId))
+                    break;
+                else
+                    Console.WriteLine("Input must be a number!");
+            }
+
+            studentService.GetAllByGroupId(groupId);
+            Console.ReadKey();
+        }
+        private void SearchStudentByNameOrSurname(StudentService studentService)
+        {
+            Console.WriteLine("_______Serach students by name/surname_______");
+
+            Console.Write("Enter a name/surname: ");
+            string keyword = Console.ReadLine();
+            var students = studentService.GetAll().Where(s => s.name.ToLower().Contains(keyword.ToLower()) ||
+            s.surname.ToLower().Contains(keyword.ToLower())).ToList();
+
+            if (students.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No student found.");
+            else
+            {
+                foreach (var s in students)
+                    Console.WriteLine($"Name :{s.name} | Surname: {s.surname} | Age: {s.age} | Group: {s.group.name} ");
+            }
+            Console.ReadKey();
+        }
+
+        private void GetAllStudents(StudentService studentService)
+        {
+            Console.WriteLine("_______All Students_______");
+
+            var allstudents = studentService.GetAll();
+
+            if (allstudents.Count == 0)
+                Helpers.ConsoleColor(ConsoleColor.Red, "No student found.");
+
+            else
+            {
+                foreach (var s in allstudents)
+                    Console.WriteLine($"Name :{s.name} | Surname: {s.surname} | Age: {s.age} | Group: {s.group.name} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
